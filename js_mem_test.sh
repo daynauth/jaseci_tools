cfce98a89901dca8cf7e4d1676a62d2cf0fb6265
daynauth <daynauth@gmail.com>
2022-07-08 17:03:05 -0400

    updated more of js manifest

--- js_mem_test.sh
+++ js_mem_test.sh
@@ -1,2 +1,15 @@
- pod_name=$(kubectl get pods | grep -Eo '^jaseci-\w{10}-\w{5}')
- kubectl exec $pod_name -- cat /sys/fs/cgroup/memory/memory.usage_in_bytes 
+line=$(kubectl get pods | grep -E '^jaseci-\w{10}-\w{5}')
+status=$(echo "$line" | tr -s ' ' | cut -d ' ' -f 3)
+echo $status
+pod_name=$(echo "$line" | tr -s ' ' | cut -d ' ' -f 1)
+
+while [ $status=="Running" ]
+do
+    #pod_name=$(kubectl get pods | grep -Eo '^jaseci-\w{10}-\w{5}')
+    bytes=$(kubectl exec $pod_name -- cat /sys/fs/cgroup/memory/memory.max_usage_in_bytes)
+    echo $(($bytes/1000000)), $status
+
+    line=$(kubectl get pods | grep -E '^jaseci-\w{10}-\w{5}')
+    status=$(echo "$line" | tr -s ' ' | cut -d ' ' -f 3)
+    pod_name=$(echo "$line" | tr -s ' ' | cut -d ' ' -f 1)
+done
